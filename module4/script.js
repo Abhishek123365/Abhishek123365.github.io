0d561e8277b66034f9ef89825c27f7102d10ecbf
Abhishek123365 <abhishek123singh365@gmail.com>
2021-05-24 11:40:37 +0530

    module4

--- module4/script.js
+++ module4/script.js
@@ -1,21 +1,17 @@
-var names=new Array();
-names[0]="Yashwant";
-names[1]="Jay";
-names[2]="Jenning";
-names[3]="jagatraj";
-names[4]="paul";
-names[5]="franklin";
-names[6]="larryom";
-names[7]="paulas";
-names[8]="laurad";
-names[9]="jimmy";
 
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
-	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
-	}
-	else{
-		console.log("Hello "+ names[i])
-	}
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
 }
+
+})();
